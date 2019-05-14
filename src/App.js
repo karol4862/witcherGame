c3d10683609044baa3c45e737a1a4eec4652d7b6
Karol Makara <karol_makara17@wp.pl>
2019-05-14 20:04:32 +0200

    create files, create first page, create styles

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
+import Hero from './pages/Hero';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  state = {
+    gold: 1000,
+    armor: null,
+    sword: null
+  }
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <nav>
+            <ul>
+              <NavLink to="/" exact><li></li></NavLink>
+              <NavLink to="/store"><li>Gold: {this.state.gold}</li></NavLink> 
+              <NavLink to="/adventure"><li></li></NavLink> 
+            </ul>
+          </nav>
+          <Route path="/" render= {()=> <Hero />}/>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
+
---
+++ src/pages/Hero.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../styles/Hero.css';
+
+const Hero = () => {
+    return ( 
+        <main className="hero">
+            <h2>My equipment</h2>
+            
+            <section>
+                IMG weapon <br/>
+                name <br/>
+                DMG 
+            </section>
+            <section> 
+                IMG armor<br/>
+                name <br/>
+                DMG 
+            </section>
+
+        </main>
+    );
+}
+ 
+export default Hero;
