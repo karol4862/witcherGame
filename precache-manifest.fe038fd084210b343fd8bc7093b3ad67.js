4ccdbd557cbae362305c50ba7d96a63f66ade451
Karol Makara <karol_makara17@wp.pl>
2019-06-05 20:26:52 +0200

    Updates

---
+++ precache-manifest.fe038fd084210b343fd8bc7093b3ad67.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6936aa1627dcb39ff398ac38dfd9b362",
+    "url": "/witcherGame/index.html"
+  },
+  {
+    "revision": "76e1d041566f2045d258",
+    "url": "/witcherGame/static/css/main.9b048ac2.chunk.css"
+  },
+  {
+    "revision": "5c8346b44100b25d3387",
+    "url": "/witcherGame/static/js/2.4af1e388.chunk.js"
+  },
+  {
+    "revision": "76e1d041566f2045d258",
+    "url": "/witcherGame/static/js/main.936410c4.chunk.js"
+  },
+  {
+    "revision": "f03e71917c8a392a16d0",
+    "url": "/witcherGame/static/js/runtime~main.3f13aaed.js"
+  },
+  {
+    "revision": "d067cf6d928963f1f4249802d416be3f",
+    "url": "/witcherGame/static/media/backgroundhome.d067cf6d.jpg"
+  },
+  {
+    "revision": "e7f5cf03111524cba46300ee43ea8be1",
+    "url": "/witcherGame/static/media/hand.e7f5cf03.png"
+  },
+  {
+    "revision": "cc89f9cb9c1381a27d849241eebda7eb",
+    "url": "/witcherGame/static/media/sword.cc89f9cb.png"
+  },
+  {
+    "revision": "a91d0f9626c7aa7b5b2f4d61c5ef465b",
+    "url": "/witcherGame/static/media/witcherpic.a91d0f96.jpg"
+  }
+]);
